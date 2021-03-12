a330e244e29d42f3e88cf0dedc36ba3b0d889061
Vanya_G <vanyagafyak@gmail.com>
2021-03-13 00:58:08 +0200

    feat: add empty create game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { GameListComponent } from './components/game-list/game-list.component';
 import { RoutingModule } from './routing.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CreateGameComponent } from './components/create-game/create-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameListComponent,
+    CreateGameComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { GameListComponent } from './components/game-list/game-list.component';
-import { LoginFormComponent } from './auth/login-form/login-form.component';
-import { RegisterFormComponent } from './auth/register-form/register-form.component';
+import { CreateGameComponent } from './components/create-game/create-game.component';
 
 const routes: Routes = [
   {
     path: 'games',
     component: GameListComponent,
+  },
+  {
+    path: 'create-game',
+    component: CreateGameComponent,
   },
   {
     path: 'auth',
